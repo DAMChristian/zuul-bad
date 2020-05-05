823bc25a0d463bbda8994e31852177e0b69b2bed
Christian <zohanyo@gmail.com>
2020-05-05 12:20:42 +0200

    12. Arreglando bugs

--- Game.java
+++ Game.java
@@ -153,11 +153,11 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("Estas perdido. Estas solo. Estas vagando");
+        System.out.println("por la tumba del faraon.");
         System.out.println();
-        System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("Tus comandos son:");
+        parser.showCommands();
     }
 
     /** 
